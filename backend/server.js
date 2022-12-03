2935ab4671aa0370c256ef6a2976b1cec0e56fa0
Christoffer Helgemo <cchristoffer@live.se>
2022-12-03 13:22:48 +0100

    Skeleton work

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,24 @@
-console.log("Server...")
+const express = require('express')
+const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middlewares/errorMiddleware')
+const PORT = process.env.PORT || 8000
+
+const app = express()
+
+//Middlewares
+
+//Enables JSON
+app.use(express.json())
+//Enables url encoded bodies
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Welcome to the Help desk api'})
+})
+
+//Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
+
+app.listen(PORT, () => {console.log(`Server started on port ${PORT}`)})
