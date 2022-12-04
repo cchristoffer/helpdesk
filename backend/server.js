53e86a97f6ae84435184bffff69f0507efb23cc1
Christoffer Helgemo <cchristoffer@live.se>
2022-12-04 17:38:07 +0100

    test

--- backend/server.js
+++ backend/server.js
@@ -26,19 +26,32 @@ app.get("/", (req, res) => {
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/tickets", require("./routes/ticketRoutes"));
 
-// Serve frontend
-if (process.env.NODE_ENV === "production") {
-  //Set build folder as static
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(__dirname, "../", "frontend", "build", "index.html")
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "../frontend/build/index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
   );
-} else {
-  app.get("/", (req, res) => {
-    res.status(200).json({ message: "Welcome to the Help desk api" });
-  });
-}
+});
+
+// // Serve frontend
+// if (process.env.NODE_ENV === "production") {
+//   //Set build folder as static
+//   app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+//   app.get("*", (req, res) =>
+//     res.sendFile(__dirname, "../", "frontend", "build", "index.html")
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.status(200).json({ message: "Welcome to the Help desk api" });
+//   });
+// }
 
 app.use(errorHandler);
 
